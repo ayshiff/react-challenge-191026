14ba525c6b82416c47dcdb5e1c82908b7f70d071
Rémi DOREAU <remi.d45@gmail.com>
2019-11-07 22:05:47 +0100

    feat(redux): add epic middleware

---
+++ client/src/epics/index.ts
@@ -0,0 +1,11 @@
+import { combineEpics } from "redux-observable";
+
+import studentEpic from "./student.epic";
+import teacherEpic from "./teacher.epic";
+
+const epics = combineEpics(
+  ...studentEpic,
+  ...teacherEpic
+);
+
+export default epics;
