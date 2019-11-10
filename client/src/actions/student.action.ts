1541e3fe3060608dbb8c726cfc07944ac8142baf
Rémi DOREAU <remi.d45@gmail.com>
2019-11-10 16:31:32 +0100

    feat(cliet): update actions

--- client/src/actions/student.action.ts
+++ client/src/actions/student.action.ts
@@ -1,57 +1,61 @@
 export interface Student {
-    name: string,
-    surname: string,
-    description: string,
-    email: string,
+  name: string;
+  surname: string;
+  description: string;
+  email: string;
 }
 
-export const StudentActions =  {
-    ADD_STUDENT: "[STUDENT] ADD_STUDENT",
-    EDIT_STUDENT: "[STUDENT] EDIT_STUDENT",
-    DELETE_STUDENT: "[STUDENT] DELETE_STUDENT",
-    GET_STUDENT: "[STUDENT] GET_STUDENT",
-    ADD_STUDENT_SUCCESS: "[STUDENT] ADD_STUDEN_SUCCESST",
-    EDIT_STUDENT_SUCCESS: "[STUDENT] EDIT_STUDENT_SUCCESS",
-    DELETE_STUDENT_SUCCESS: "[STUDENT] DELETE_STUDENT_SUCCESS",
-    GET_STUDENT_SUCCESS: "[STUDENT] GET_STUDENT_SUCCESS"
-}
+export const StudentActions = {
+  ADD_STUDENT: "[STUDENT] ADD_STUDENT",
+  EDIT_STUDENT: "[STUDENT] EDIT_STUDENT",
+  DELETE_STUDENT: "[STUDENT] DELETE_STUDENT",
+  GET_STUDENT: "[STUDENT] GET_STUDENT",
+  ADD_STUDENT_SUCCESS: "[STUDENT] ADD_STUDEN_SUCCESST",
+  EDIT_STUDENT_SUCCESS: "[STUDENT] EDIT_STUDENT_SUCCESS",
+  DELETE_STUDENT_SUCCESS: "[STUDENT] DELETE_STUDENT_SUCCESS",
+  GET_STUDENT_SUCCESS: "[STUDENT] GET_STUDENT_SUCCESS"
+};
 
-export const AAddStudent = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.ADD_STUDENT,
-    payload
-})
+export const AAddStudent = (
+  payload: Student
+): { action: string; payload: any } => ({
+  action: StudentActions.ADD_STUDENT,
+  payload
+});
 
-export const AEditStudnet = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.EDIT_STUDENT,
-    payload
-})
+export const AEditStudnet = (
+  payload: Student
+): { action: string; payload: any } => ({
+  action: StudentActions.EDIT_STUDENT,
+  payload
+});
 
-export const ADeleteStudent = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.DELETE_STUDENT,
-    payload
-})
+export const ADeleteStudent = (
+  payload: Student
+): { action: string; payload: any } => ({
+  action: StudentActions.DELETE_STUDENT,
+  payload
+});
 
-export const AGetStudents = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.GET_STUDENT,
-    payload
-})
+export const AGetStudents = (
+  payload: Student
+): { action: string; payload: any } => ({
+  action: StudentActions.GET_STUDENT,
+  payload
+});
 
-export const AAddStudentSucess = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.ADD_STUDENT_SUCCESS,
-    payload
-})
+export const AAddStudentSucess = (): { action: string } => ({
+  action: StudentActions.ADD_STUDENT_SUCCESS
+});
 
-export const AEditStudnetSucess = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.EDIT_STUDENT_SUCCESS,
-    payload
-})
+export const AEditStudnetSucess = (): { action: string } => ({
+  action: StudentActions.EDIT_STUDENT_SUCCESS
+});
 
-export const ADeleteStudentSucess = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.DELETE_STUDENT_SUCCESS,
-    payload
-})
+export const ADeleteStudentSucess = (): { action: string } => ({
+  action: StudentActions.DELETE_STUDENT_SUCCESS
+});
 
-export const AGetStudentsSucess = (payload: Student): {action: string, payload: any} => ({
-    action: StudentActions.GET_STUDENT_SUCCESS,
-    payload
-})
+export const AGetStudentsSucess = (): { action: string } => ({
+  action: StudentActions.GET_STUDENT_SUCCESS
+});
--- client/src/actions/teacher.action.ts
+++ client/src/actions/teacher.action.ts
@@ -1,27 +1,61 @@
 export interface Teacher {
-    name: string,
-    surname: string,
-    description: string,
-    email: string,
+  name: string;
+  surname: string;
+  description: string;
+  email: string;
 }
 
-const StudentActions =  {
-    ADD_TEACHER: "[TEACHER] ADD_STUDENT",
-    EDIT_TEACHER: "[TEACHER] EDIT_STUDENT",
-    DELETE_TEACHER: "[TEACHER] DELETE_STUDENT"
-}
+export const TeacherActions = {
+  ADD_TEACHER: "[TEACHER] ADD_TEACHER",
+  EDIT_TEACHER: "[TEACHER] EDIT_TEACHER",
+  DELETE_TEACHER: "[TEACHER] DELETE_TEACHER",
+  GET_TEACHER: "[TEACHER] GET_TEACHER",
+  ADD_TEACHER_SUCCESS: "[TEACHER] ADD_STUDEN_SUCCESST",
+  EDIT_TEACHER_SUCCESS: "[TEACHER] EDIT_TEACHER_SUCCESS",
+  DELETE_TEACHER_SUCCESS: "[TEACHER] DELETE_TEACHER_SUCCESS",
+  GET_TEACHER_SUCCESS: "[TEACHER] GET_TEACHER_SUCCESS"
+};
 
-export const AAddStudent = (payload: Teacher): {action: string, payload: any} => ({
-    action: StudentActions.ADD_TEACHER,
-    payload
-})
+export const AAddTeacher = (
+  payload: Teacher
+): { action: string; payload: any } => ({
+  action: TeacherActions.ADD_TEACHER,
+  payload
+});
 
-export const AEditStudnet = (payload: Teacher): {action: string, payload: any} => ({
-    action: StudentActions.EDIT_TEACHER,
-    payload
-})
+export const AEditTeacher = (
+  payload: Teacher
+): { action: string; payload: any } => ({
+  action: TeacherActions.EDIT_TEACHER,
+  payload
+});
 
-export const ADeleteStudent = (payload: Teacher): {action: string, payload: any} => ({
-    action: StudentActions.DELETE_TEACHER,
-    payload
-})
+export const ADeleteTeacher = (
+  payload: Teacher
+): { action: string; payload: any } => ({
+  action: TeacherActions.DELETE_TEACHER,
+  payload
+});
+
+export const AGetTeachers = (
+  payload: Teacher
+): { action: string; payload: any } => ({
+  action: TeacherActions.GET_TEACHER,
+  payload
+});
+
+export const AAddTeacherSucess = (): { action: string } => ({
+  action: TeacherActions.ADD_TEACHER_SUCCESS
+});
+
+export const AEditStudnetSucess = (): { action: string } => ({
+  action: TeacherActions.EDIT_TEACHER_SUCCESS
+});
+
+export const ADeleteTeacherSucess = (): { action: string } => ({
+  action: TeacherActions.DELETE_TEACHER_SUCCESS
+});
+
+export const AGetTeachersSucess = (): { action: string } => ({
+  action: TeacherActions.GET_TEACHER_SUCCESS
+});
