b8f9c190faa5c078a398fed41a51826a3c52e370
Rémi DOREAU <remi.d45@gmail.com>
2019-11-12 09:45:44 +0100

    feat(client): add missing epics

--- client/src/epics/teacher.epic.ts
+++ client/src/epics/teacher.epic.ts
@@ -2,32 +2,58 @@ import { ajax } from "rxjs/ajax";
 import { map, mergeMap } from "rxjs/operators";
 
 import {
-  AAddTeacher,
-  AEditTeacher,
-  ADeleteTeacher,
   TeacherActions,
+  AAddTeacherSucess,
+  ADeleteTeacherSucess,
   AGetTeachersSucess,
-  AAddTeacherSucess
+  AEditTeacherSucess
 } from "../actions/teacher.action";
 import { ofType } from "redux-observable";
 
+const API_URL = "https://api.github.com";
+
 const fetchTeachersEpic = (action$: any) =>
   action$.pipe(
-    ofType(AAddTeacher)
-    // mergeMap((action: any) =>
-    //   ajax
-    //     .getJSON(`https://api.github.com/users/${action.payload}`)
-    //     .pipe(map(response => AGetTeachersSucess()))
-    // )
+    ofType(TeacherActions.GET_TEACHER),
+    mergeMap((action: any) =>
+      ajax
+        .getJSON(`${API_URL}/teachers`)
+        .pipe(map(response => AGetTeachersSucess(action.payload)))
+    )
   );
 
 const addTeacherEpic = (action$: any) =>
   action$.pipe(
-    ofType(AAddTeacher)
-    // mergeMap((action: any) =>
-    //   ajax
-    //     .getJSON(`https://api.github.com/users/${action.payload}`)
-    //     .pipe(map(response => AAddTeacherSucess()))
-    // )
+    ofType(TeacherActions.ADD_TEACHER),
+    mergeMap((action: any) =>
+      ajax
+        .getJSON(`${API_URL}/teachers/add/${action.payload}`)
+        .pipe(map(response => AAddTeacherSucess(action.payload)))
+    )
   );
-export default [fetchTeachersEpic, addTeacherEpic];
+
+const deleteTeacherEpic = (action$: any) =>
+  action$.pipe(
+    ofType(TeacherActions.DELETE_TEACHER),
+    mergeMap((action: any) =>
+      ajax
+        .getJSON(`${API_URL}/teachers/delete/${action.payload}`)
+        .pipe(map(response => ADeleteTeacherSucess(action.payload)))
+    )
+  );
+
+const editStudentEpic = (action$: any) =>
+  action$.pipe(
+    ofType(TeacherActions.EDIT_TEACHER),
+    mergeMap((action: any) =>
+      ajax
+        .getJSON(`${API_URL}/teachers/edit/${action.payload}`)
+        .pipe(map(response => AEditTeacherSucess(action.payload)))
+    )
+  );
+export default [
+  fetchTeachersEpic,
+  addTeacherEpic,
+  deleteTeacherEpic,
+  editStudentEpic
+];
