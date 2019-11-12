aa299138fe97efa5f66ea6a2f12ae2bf9238b3e1
Rémi DOREAU <remi.d45@gmail.com>
2019-11-12 15:59:37 +0100

    feat(client): add LOGIN_FAIL catch in reducer

--- client/src/reducers/login.reducer.ts
+++ client/src/reducers/login.reducer.ts
@@ -1,7 +1,8 @@
 import { LoginActions } from "../actions/login.action";
 
 const initialState: any = {
-  auth: false
+  auth: false,
+  message: ""
 };
 
 export default (state = initialState, action: any) => {
@@ -9,6 +10,11 @@ export default (state = initialState, action: any) => {
     case LoginActions.LOGIN_SUCCESS:
       return {
         auth: true
+      };
+    case LoginActions.LOGIN_FAIL:
+      return {
+        auth: true,
+        message: action.payload.message
       };
     default:
       return state;
