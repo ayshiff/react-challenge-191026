c74b0da6e4191fd92850dc039d6f366aaa65f296
Rémi DOREAU <remi.d45@gmail.com>
2019-11-12 14:22:28 +0100

    feat(client): combine new reducers

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,8 +1,12 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import studentReducer from "./student.reducer";
 import teacherReducer from "./teacher.reducer";
+import loginReducer from "./login.reducer";
+import promoReducer from "./promo.reducer";
 
-export default combineReducers <any>({
-    student: studentReducer,
-    teacher: teacherReducer
-})
+export default combineReducers<any>({
+  student: studentReducer,
+  teacher: teacherReducer,
+  login: loginReducer,
+  promo: promoReducer
+});
