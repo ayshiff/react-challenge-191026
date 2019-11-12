79cc566a2bfd52cc305cc815f581610cf210f1d5
Rémi DOREAU <remi.d45@gmail.com>
2019-11-12 09:45:29 +0100

    feat(client): add missing epics

--- client/src/actions/student.action.ts
+++ client/src/actions/student.action.ts
@@ -1,4 +1,5 @@
 export interface Student {
+  id: string;
   name: string;
   ux: string;
   ui: string;
--- client/src/epics/student.epic.ts
+++ client/src/epics/student.epic.ts
@@ -2,21 +2,22 @@ import { ajax } from "rxjs/ajax";
 import { map, mergeMap, mapTo, delay } from "rxjs/operators";
 
 import {
-  AAddStudent,
-  AEditStudnet,
-  ADeleteStudent,
   StudentActions,
   AGetStudentsSucess,
   AAddStudentSucess,
-  AGetStudents
+  AEditStudnetSucess,
+  ADeleteStudentSucess
 } from "../actions/student.action";
 import { ofType } from "redux-observable";
+
+const API_URL = "https://api.github.com";
 
 const fetchStudentsEpic = (action$: any) => {
   return action$.pipe(
     ofType(StudentActions.GET_STUDENT),
     mapTo(
       AGetStudentsSucess({
+        id: "test",
         name: "ok",
         ux: "A",
         ui: "A",
@@ -39,4 +40,29 @@ const addStudentEpic = (action$: any) =>
     })
   );
 
-export default [fetchStudentsEpic, addStudentEpic];
+const deleteStudentEpic = (action$: any) =>
+  action$.pipe(
+    ofType(StudentActions.DELETE_STUDENT),
+    mergeMap((action: any) =>
+      ajax
+        .getJSON(`${API_URL}/student/${action.payload}`)
+        .pipe(map(response => ADeleteStudentSucess(action.payload)))
+    )
+  );
+
+const editStudentEpic = (action$: any) =>
+  action$.pipe(
+    ofType(StudentActions.EDIT_STUDENT),
+    mergeMap((action: any) =>
+      ajax
+        .getJSON(`${API_URL}/student/${action.payload}`)
+        .pipe(map(response => AEditStudnetSucess(action.payload)))
+    )
+  );
+
+export default [
+  fetchStudentsEpic,
+  addStudentEpic,
+  deleteStudentEpic,
+  editStudentEpic
+];
