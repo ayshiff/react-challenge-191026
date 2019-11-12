d37302b21c2805cfb4d3600203809b99a87e8e50
Rémi DOREAU <remi.d45@gmail.com>
2019-11-12 21:21:57 +0100

    feat(client): add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Dashboard from "./components/dashboard.component";
-import Login from "./components/login.component";
+import Overview from "./components/overview.component";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/promo">
+            <Overview />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
