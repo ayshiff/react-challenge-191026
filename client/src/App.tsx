3cf9d38f849301196b2a0a902f4308be487993c4
Rémi DOREAU <remi.d45@gmail.com>
2019-11-14 09:36:10 +0100

    feat(client): add create_promo component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Dashboard from "./components/dashboard.component";
 import Overview from "./components/overview.component";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreatePromo from "./components/create_promo.component";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/promo">
-            <Overview />
-          </Route>
+          <Route
+            path="/dashboard"
+            render={(props: any) => <Dashboard {...props} />}
+          />
+          <Route
+            path="/promo"
+            render={(props: any) => <Overview {...props} />}
+          />
+          <Route
+            path="/create_promo"
+            render={(props: any) => <CreatePromo {...props} />}
+          />
         </Switch>
       </Router>
     </div>
