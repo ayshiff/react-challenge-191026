1e4261f68be467c09e9294fc39ff1f58a208f633
Rémi DOREAU <remi.d45@gmail.com>
2019-11-14 09:36:37 +0100

    feat(client): add antd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
 
 import { Provider } from "react-redux";
 import { Store } from "./config/store";
