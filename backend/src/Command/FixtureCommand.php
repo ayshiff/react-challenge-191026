be480c9b1ec53d40b289ae6e96ea884a6bff11f0
Corentin <corentin.croizat@onbrane.com>
2019-11-14 15:00:55 +0100

    added fixtures

--- backend/src/Command/FixtureCommand.php
+++ backend/src/Command/FixtureCommand.php
@@ -3,7 +3,6 @@
 namespace App\Command;
 
 use App\Entity\Cursus;
-use App\Entity\Student;
 use App\Entity\Teacher;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,7 +10,6 @@ use Faker\Factory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class FixtureCommand extends Command
 {
@@ -20,14 +18,11 @@ class FixtureCommand extends Command
 
     protected $entityManager;
 
-    protected $passwordEncoder;
-
     protected $faker;
 
-    public function __construct(EntityManagerInterface $manager, UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(EntityManagerInterface $manager)
     {
         $this->entityManager = $manager;
-        $this->passwordEncoder = $passwordEncoder;
         $this->faker = Factory::create();
         parent::__construct();
     }
@@ -68,10 +63,7 @@ class FixtureCommand extends Command
         // admin
         $user = new User();
         $user->setMail('admin@hetic.net');
-        $user->setPassword($this->passwordEncoder->encodePassword(
-            $user,
-            'admin'
-        ));
+        $user->setPassword('admin');
         $user->setRoles(['ROLE_ADMIN']);
         $this->entityManager->persist($user);
 
@@ -79,10 +71,7 @@ class FixtureCommand extends Command
         // teacher
         $user = new User();
         $user->setMail('eric.priou@hetic.net');
-        $user->setPassword($this->passwordEncoder->encodePassword(
-            $user,
-            'teacher'
-        ));
+        $user->setPassword('teacher');
         $user->setRoles(['ROLE_TEACHER']);
         $this->entityManager->persist($user);
 
@@ -93,25 +82,25 @@ class FixtureCommand extends Command
         $this->entityManager->persist($teacher);
 
         // students
-        for ($i = 1; $i <= 10; $i++) {
-            $firstname = $this->faker->firstName;
-            $lastname = $this->faker->lastName;
-            $user = new User();
-            $user->setMail($firstname.'.'.$lastname.'@hetic.net');
-            $user->setPassword($this->passwordEncoder->encodePassword(
-                $user,
-                'fixture'
-            ));
-            $user->setRoles(['ROLE_STUDENT']);
-            $this->entityManager->persist($user);
-
-            $student = new Student();
-            $student->setFirstname($firstname);
-            $student->setLastname($lastname);
-            $student->setBirthdate($this->faker->dateTime(new \DateTime('2000-01-01')));
-            $student->setUser($user);
-            $this->entityManager->persist($student);
-        }
+//        for ($i = 1; $i <= 10; $i++) {
+//            $firstname = $this->faker->firstName;
+//            $lastname = $this->faker->lastName;
+//            $user = new User();
+//            $user->setMail($firstname.'.'.$lastname.'@hetic.net');
+//            $user->setPassword($this->passwordEncoder->encodePassword(
+//                $user,
+//                'fixture'
+//            ));
+//            $user->setRoles(['ROLE_STUDENT']);
+//            $this->entityManager->persist($user);
+//
+//            $student = new Student();
+//            $student->setFirstname($firstname);
+//            $student->setLastname($lastname);
+//            $student->setBirthdate($this->faker->dateTime(new \DateTime('2000-01-01')));
+//            $student->setUser($user);
+//            $this->entityManager->persist($student);
+//        }
         $this->entityManager->flush();
     }
 
